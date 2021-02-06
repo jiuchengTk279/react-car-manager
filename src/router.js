dbdb3d809de865d9bfdbe4f51f7abe8e29e5dd3c
jiucheng <15279232381@163.com>
2021-02-06 16:01:52 +0800

    完成UI菜单中轮播图的功能

--- src/router.js
+++ src/router.js
@@ -10,6 +10,7 @@ import Notice from './pages/ui/notice'
 import Message from './pages/ui/message'
 import Tabs from './pages/ui/tabs'
 import Gallery from './pages/ui/gallery'
+import Carousel from './pages/ui/carousel'
 import NoMatch from './pages/nomatch'
 
 export default class IRouter extends Component {
@@ -29,6 +30,7 @@ export default class IRouter extends Component {
                 <Route path="/admin/ui/messages" component={Message}></Route>
                 <Route path="/admin/ui/tabs" component={Tabs}></Route>
                 <Route path="/admin/ui/gallery" component={Gallery}></Route>
+                <Route path="/admin/ui/carousel" component={Carousel}></Route>
                 <Route component={NoMatch}></Route>
               </Switch>
             </Admin>
